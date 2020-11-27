51d0da756c28560a866d9fc58e51f5fcb04d82db
Gasper Pust <gapi.pust@gmail.com>
2020-11-28 00:25:57 +0100

    jarvis march algoritem

--- algoritem.py
+++ algoritem.py
@@ -74,3 +74,87 @@ mreza = mreza(4,4)
 #nova_mreza = list(set(mreza) - set(graham_scan(mreza)))
 
 #print(graham_scan(nova_mreza))
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def subtract(self, p):
+    	return Point(self.x - p.x, self.y - p.y)
+
+    def __str__(self):
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+    
+def distance_sq(p1, p2):
+    return (p1.x - p2.x)**2 + (p1.y - p2.y)**2
+# calculates the cross product of vector p1 and p2
+# if p1 is clockwise from p2 wrt origin then it returns +ve value
+# if p2 is anti-clockwise from p2 wrt origin then it returns -ve value
+# if p1 p2 and origin are collinear then it returs 0
+def cross_product(p1, p2):
+	return p1.x * p2.y - p2.x * p1.y
+# returns the cross product of vector p1p3 and p1p2
+# if p1p3 is clockwise from p1p2 it returns +ve value
+# if p1p3 is anti-clockwise from p1p2 it returns -ve value
+# if p1 p2 and p3 are collinear it returns 0
+def direction(p1, p2, p3):
+	return  cross_product(p3.subtract(p1), p2.subtract(p1))
+
+
+def jarvis_march(seznam):
+    zacetna_tocka =  min(seznam, key = lambda tocka: tocka.x) #poiščemo najbolj levo točko
+    indeks = seznam.index(zacetna_tocka)
+    
+    l = indeks
+    rezultat = []
+    rezultat.append(zacetna_tocka)
+    while (True):
+        q = (l + 1) % len(seznam)
+        for i in range(len(seznam)):
+            if i == l:
+                continue
+            d = direction(seznam[l], seznam[i], seznam[q]) #poiščemo največji levi ovinek, v primeru kolinearnosti vključimo točko, ki je najdlje
+            if d > 0 or (d == 0 and distance_sq(seznam[i], seznam[l]) > distance_sq(seznam[q], seznam[l])):
+                q = i
+        l = q
+        if l == indeks: #pridemo nazaj na začetek
+            break 
+        rezultat.append(seznam[q])
+
+    return rezultat
+
+#print(jarvis_march(mreza))
+
+#tuki sm probu spremenit da se ne navezuje na class Point
+
+def distance_sq(p1, p2):
+    return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+
+def cross_product(p1, p2):
+	return p1[0] * p2[1] - p2[0] * p1[1]
+
+def direction(p1, p2, p3):
+    return cross_product(p3[0]-p1[0], p3[1]-p1[1])
+
+def jarvis_march(seznam):
+    zacetna_tocka =  min(seznam, key = lambda tocka: tocka[0]) #poiščemo najbolj levo točko
+    indeks = seznam.index(zacetna_tocka)
+    
+    l = indeks
+    rezultat = []
+    rezultat.append(zacetna_tocka)
+    while (True):
+        q = (l + 1) % len(seznam)
+        for i in range(len(seznam)):
+            if i == l:
+                continue
+            d = direction(seznam[l], seznam[i], seznam[q]) #poiščemo največji levi ovinek, v primeru kolinearnosti vključimo točko, ki je najdlje
+            if d > 0 or (d == 0 and distance_sq(seznam[i], seznam[l]) > distance_sq(seznam[q], seznam[l])):
+                q = i
+        l = q
+        if l == indeks: #pridemo nazaj na začetek
+            break 
+        rezultat.append(seznam[q])
+
+    return rezultat
