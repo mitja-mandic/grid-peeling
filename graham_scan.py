a56b6e837017cbe2d32e9fcdfe97d47d54ddb248
Gasper Pust <gapi.pust@gmail.com>
2020-12-03 21:56:40 +0100

    dopolnjena vizualizacija in manjši popravki

--- graham_scan.py
+++ graham_scan.py
@@ -1,65 +1,27 @@
 from razredi import Tocka, naredi_neenakomerno, smer_razlike, naredi_potencno
 import time
 
-#sez = [Tocka(i,j) for i in range(4) for j in range(4)]
-
-#def kot_med_tockama(tocka1, tocka2):
-#    if tocka1[1] == tocka2[1]:
-#        return 0.0
-#    elif tocka1[0] == tocka2[0]:
-#        return 90
-#    else:
-#        return (tocka2[1] - tocka1[1])/(tocka2[0] - tocka1[0])
-
 def uredi_po_kotu(seznam):
     prvi = min(seznam, key = lambda tocka: (tocka.x, tocka.y))
     return [prvi] + sorted(seznam[1:], key=lambda x: prvi.kot_med_dvema(x))
 
-#[print(x) for x in uredi_po_kotu_classi(sez)]
-
-
-#def uredi_po_kotu(seznam):
-#    """
-#    sprejme seznam točk, ga uredi po kotu glede na x-os.
-#    """
-#    prvi = seznam[0]
-#    return [prvi] + sorted(seznam[1:], key=lambda x: kot_med_tockama(prvi,x))
-
-
-#LEVO, DESNO, KOLINEARNE = 1,-1,0
-
-def graham_scan(seznam):
-
-    #def cmp(a, b):
-    #    return (a > b) - (a < b)
-
-    #def ovinek(p, q, r):
-    #    return (q[0] - p[0])*(r[1] - p[1]) - (r[0] - p[0])*(q[1] - p[1])
-    
+def graham_scan(seznam):   
     urejene_tocke = uredi_po_kotu(seznam)
-    #print(urejene_tocke)
-
     ovojnica = [] #stack v katerega dodajamo točke
 
     for tocka in urejene_tocke:
-
-        while len(ovojnica) > 1 and smer_razlike(ovojnica[-2], ovojnica[-1], tocka) >= 0:
-            
+        while len(ovojnica) > 1 and smer_razlike(ovojnica[-2], ovojnica[-1], tocka) >= 0:           
             ovojnica.pop()
-
         ovojnica.append(tocka)
-
     return ovojnica
 
 def grid_peel_graham_enakomerna(m, n):
     start = time.time()
     mreza = [Tocka(i,j) for i in range(m) for j in range(n)]
-
     ovojnice = {}
     i = 0
     while mreza or len(mreza) > 1:
         ch = graham_scan(mreza)
-        #print(ch)
         nova = [x for x in mreza if x not in ch]
         mreza = nova
         ovojnice[i] = ch
@@ -70,21 +32,16 @@ def grid_peel_graham_enakomerna(m, n):
 def grid_peel_graham_potencna(m, n):
     start = time.time()
     mreza = naredi_potencno(m, n)
-
     ovojnice = {}
     i = 0
     while mreza or len(mreza) > 1:
         ch = graham_scan(mreza)
-        #print(ch)
         nova = [x for x in mreza if x not in ch]
         mreza = nova
-        i += 1
         ovojnice[i] = ch
+        i += 1
     casovna_zahtevnost = time.time() - start 
     return i, ovojnice, casovna_zahtevnost
 
-#print(grid_peel_graham_enakomerna(50, 50))
 def tabela_graham_enakomerna(m,n):
     return [grid_peel_graham_enakomerna(i,j)[2] for i in range(m) for j in range(n)]
-
-#print(tabela(20,20))
--- razredi.py
+++ razredi.py
@@ -18,11 +18,9 @@ class Tocka:
     def vektorski_produkt(self, other): 
         return self.x * other.y - self.y * other.x
 
-
     def razlika(self, other):
     	return Tocka(self.x - other.x, self.y - other.y)
 
-    
     def razdalja(self, other):
         return (self.x - other.x) ** 2 + (self.y - other.y) ** 2
 
@@ -32,50 +30,23 @@ class Tocka:
     def __repr__(self):
         return 'T(' + str(self.x) + ', ' + str(self.y) + ')'
 
-#def vektorski_produkt1(p,q)
-
 
 def smer_razlike(p,q,r):
     return p.razlika(q).vektorski_produkt(r.razlika(q))
 
-
-
-
-#p = Tocka(0,0)
-#q = Tocka(1,0.5)
-#r = Tocka(2,1)
-
-#print(smer_razlike(p,q,r))
-
-
-
-
-
-
-
 def naredi_neenakomerno(st_tock, zgornja_meja):
     seznam = sorted(random.sample(range(zgornja_meja),st_tock))
- 
- #   print()
     return [Tocka(i,j) for i in seznam for j in seznam]
-#print([naredi_neenakomerno(5)])
 
 def naredi_potencno(m, n):
     return [Tocka(2**i,2**j) for i in range(m) for j in range(n)]
 
-#print(naredi_potencno(10, 10))
 
-#nevem če ta razred zares rabimo
 #class Mreza:
 #    def __init__(self, m, n):#, vrsta):
 #        self.m = m 
 #        self.n = n
-#    #    self.vrsta = str(vrsta)
+#        self.vrsta = str(vrsta)
 #    
 #    def mreza(self):
 #        return [Tocka(i,j) for i in range(self.m) for j in range(self.n)]
-
-#grid = Mreza(2,2)
-#
-#print(grid.mreza())
-#print(naredi_neenakomerno(3, 10))
